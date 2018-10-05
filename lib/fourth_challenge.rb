8c569cfecb3d3b4d2f4a99cf6ec3a1e53ea0f699
scottyd_14 <scottyd_14@hotmail.com>
2018-10-05 17:22:28 +0000

    automatically backed up by learn

--- lib/fourth_challenge.rb
+++ lib/fourth_challenge.rb
@@ -9,7 +9,7 @@ def fourth_challenge
         :age => "17",
         :attitude => "worried"
    }{
-       :name => "Mercutio"},
+       :name => "Mercutio",
         :age => "18",
         :attitude => "hot-headed"
         }
